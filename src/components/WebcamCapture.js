bd29dc68b4d2fb6c83f6f9b98fd000412b4db589
Shaun <s-purslow@hotmail.com>
2021-09-09 13:35:42 -0600

    add usehistory hook to link to preview page

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -1,6 +1,10 @@
 import React, { useCallback, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import { useDispatch } from 'react-redux';
+import { setCameraImage } from '../features/cameraSlice';
+import { useHistory } from 'react-router';
+import '../styles/WebcamCapture.css'
 
 const videoConstraints = {
   width: 250,
@@ -10,12 +14,14 @@ const videoConstraints = {
 
 const WebcamCapture = () => {
   const webcamRef = useRef(null);
-  const [image, setImage] = useState(null);
+  const dispatch = useDispatch();
+    const history = useHistory();
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
-    console.log(imageSrc);
-    setImage(imageSrc)
+    dispatch(setCameraImage(imageSrc));
+    history.push('/preview')
+    
   }, [webcamRef]);
   return (
     <div className='webcamCapture'>
@@ -31,9 +37,8 @@ const WebcamCapture = () => {
       <RadioButtonUncheckedIcon
         className='webcamCapture__button'
         onClick={capture}
-        fontSize="large"
+        fontSize='large'
       />
-      <img src={image} alt="" />
     </div>
   );
 };
