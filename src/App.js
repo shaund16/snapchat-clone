12e5fd510bb357f7100b66d2e79ac97375cc0fc3
Shaun <s-purslow@hotmail.com>
2021-09-09 13:34:51 -0600

    add react router, a route for preview after picture taken, add preview component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from './components/WebcamCapture';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Preview from './components/Preview';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Snapchat</h1>
-      <WebcamCapture />
+    <div className='app'>
+      <Router>
+        <div className='app__body'>
+          <Switch>
+            <Route exact path='/preview'>
+              <Preview />
+            </Route>
+            <Route exact path='/'>
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
