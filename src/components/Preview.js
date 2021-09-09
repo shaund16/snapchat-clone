78ea452b98024328100e9fe0cb23d9fc06e9d26c
Shaun <s-purslow@hotmail.com>
2021-09-09 14:34:28 -0600

    create close preview function the redirects if no camera picture saved

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import '../styles/Preview.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectCameraImage, resetCameraImage } from '../features/cameraSlice';
+import { useHistory } from 'react-router';
+import CloseIcon from '@material-ui/icons/Close';
 
 const Preview = () => {
+  const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace('/');
+    }
+  }, [cameraImage, history]);
+
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+    history.replace('/')
+  }
+
   return (
     <div className='preview'>
-      
+      <CloseIcon onClick={closePreview} className='preview__close'/>
+      <img src={cameraImage} alt='' />
     </div>
-  )
-}
+  );
+};
 
-export default Preview
+export default Preview;
