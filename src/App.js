748d25b7755e6703a48c5728798c782219a409f6
Shaun <s-purslow@hotmail.com>
2021-09-10 12:47:54 -0600

    add phone background and style it, get snapchat logo onto of phone, make coniditonal login feature

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import WebcamCapture from './components/WebcamCapture';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Preview from './components/Preview';
 import Chats from './components/Chats';
 import ChatView from './components/ChatView';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/appSlice';
+import Login from './components/Login';
+import { auth } from './firebase';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(login({
+          username: authUser.displayName,
+          profilePic: authUser.photoURL,
+          id: authUser.uid,
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
   return (
     <div className='app'>
       <Router>
-        <div className='app__body'>
-          <Switch>
-            <Route exact path='/chats/view'>
-              <ChatView />
-            </Route>
-            <Route exact path='/chats'>
-              <Chats />
-            </Route>
-            <Route exact path='/preview'>
-              <Preview />
-            </Route>
-            <Route exact path='/'>
-              <WebcamCapture />
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+          <img className='app__logo' src="https://lakeridgenewsonline.com/wp-content/uploads/2020/04/snapchat.jpg" alt="" />
+          <div className='app__body'>
+          <div className="app__Background">
+            <Switch>
+              <Route exact path='/chats/view'>
+                <ChatView />
+              </Route>
+              <Route exact path='/chats'>
+                <Chats />
+              </Route>
+              <Route exact path='/preview'>
+                <Preview />
+              </Route>
+              <Route exact path='/'>
+                <WebcamCapture />
+              </Route>
+            </Switch>
+          </div>
+          </div>
+          </>
+        )}
       </Router>
     </div>
   );
