99df10b34b1a6efa700b32085be815d981e6f2a4
Shaun <s-purslow@hotmail.com>
2021-09-09 20:40:26 -0600

    add icons to preview component, add the send now icon and style

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -4,6 +4,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectCameraImage, resetCameraImage } from '../features/cameraSlice';
 import { useHistory } from 'react-router';
 import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
+import SendIcon from '@material-ui/icons/Send';
 
 const Preview = () => {
   const cameraImage = useSelector(selectCameraImage);
@@ -24,7 +32,20 @@ const Preview = () => {
   return (
     <div className='preview'>
       <CloseIcon onClick={closePreview} className='preview__close'/>
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
       <img src={cameraImage} alt='' />
+      <div className="preview__footer">
+        <h2>Send Now</h2>
+        <SendIcon fontSize='small' className='preview__sendIcon' />
+      </div>
     </div>
   );
 };
