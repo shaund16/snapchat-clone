6cdbf65b637a817f2496b6e7bd9135d7afcfba79
Shaun <s-purslow@hotmail.com>
2021-09-09 21:52:01 -0600

    add picture once clicking send with sendpost function to firebase

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -12,11 +12,17 @@ import AttachFileIcon from '@material-ui/icons/AttachFile';
 import CropIcon from '@material-ui/icons/Crop';
 import TimerIcon from '@material-ui/icons/Timer';
 import SendIcon from '@material-ui/icons/Send';
+import { v4 as uuid } from 'uuid';
+import { db, storage } from '../firebase';
+import firebase from 'firebase'
+
+
 
 const Preview = () => {
   const cameraImage = useSelector(selectCameraImage);
   const history = useHistory();
   const dispatch = useDispatch();
+  
 
   useEffect(() => {
     if (!cameraImage) {
@@ -26,13 +32,41 @@ const Preview = () => {
 
   const closePreview = () => {
     dispatch(resetCameraImage());
-    history.replace('/')
-  }
+    history.replace('/');
+  };
+
+  const sendPost = () => {
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`posts/${id}`)
+      .putString(cameraImage, 'data_url');
+
+    uploadTask.on(
+      'state_changed',
+      null,
+      (error) => {
+        //Error Function
+        console.log(error);
+      },
+      () => {
+        //Complete Function
+        storage.ref('posts').child(id).getDownloadURL().then((url) => {
+          db.collection('posts').add({
+            imageUrl: url,
+            username: 'Shaun',
+            read: false,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          });
+          history.replace('/chats');
+        });
+      }
+    );
+  };
 
   return (
     <div className='preview'>
-      <CloseIcon onClick={closePreview} className='preview__close'/>
-      <div className="preview__toolbarRight">
+      <CloseIcon onClick={closePreview} className='preview__close' />
+      <div className='preview__toolbarRight'>
         <TextFieldsIcon />
         <CreateIcon />
         <NoteIcon />
@@ -42,7 +76,7 @@ const Preview = () => {
         <TimerIcon />
       </div>
       <img src={cameraImage} alt='' />
-      <div className="preview__footer">
+      <div onClick={sendPost} className='preview__footer'>
         <h2>Send Now</h2>
         <SendIcon fontSize='small' className='preview__sendIcon' />
       </div>
