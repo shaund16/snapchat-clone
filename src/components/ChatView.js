b32c9f2dde8f6df365cd1127f5fc6985d7292a35
Shaun <s-purslow@hotmail.com>
2021-09-10 10:26:26 -0600

    create chatview component

---
+++ src/components/ChatView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/ChatView.css';
+
+const ChatView = () => {
+  return (
+    <div className='chatView'>
+      
+    </div>
+  )
+}
+
+export default ChatView
